af00c2d1df8361127b9e607c113716ec677f189f
Santanu-11 <singhsantanu683@gmail.com>
2026-06-28 14:19:14 +0530

    Whatsapp Number Update

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 // src/lib/constants.ts
-export const WHATSAPP_NUMBER = '6421993486' // confirm with client, no spaces/symbols, country code first
+export const WHATSAPP_NUMBER = '640221993486' // confirm with client, no spaces/symbols, country code first
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}`
