7338333b737a1750baf8b5293828928327cf322c
Santanu-11 <singhsantanu683@gmail.com>
2026-07-19 15:43:24 +0530

    GA4 added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,6 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <GoogleAnalytics gaId="G-PN533SHQG2" />
       </body>
     </html>
   )
