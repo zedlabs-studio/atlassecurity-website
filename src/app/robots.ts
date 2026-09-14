c26952c1d5e7cad5f55a784110edfc7e2e17439c
Lakshay <lakhwanlakshay7@gmail.com>
2026-09-14 22:21:09 +0530

    chore(seo): update disallow rules in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/admin/', '/api/'],
+        disallow: ['/admin/', '/api/', '/login/', '/private/', '/tmp/'],
       },
     ],
     sitemap: `${siteConfig.url}/sitemap.xml`,
